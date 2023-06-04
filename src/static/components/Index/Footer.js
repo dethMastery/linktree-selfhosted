74168329bbda5c8b36906ee499fc9b2cc35c26be
dethMastery <me@suphakit.net>
2023-06-04 17:46:22 +0700

    chore: footer updation

--- src/static/components/Index/Footer.js
+++ src/static/components/Index/Footer.js
@@ -1,5 +1,10 @@
 const Footer = `<div id="footer">
-  &copy; 2023 <a href="https://suphakit.net" target="_blank" class="link">Suphakit P.</a> All Right Reserved.
+  <p>
+    made w/ 🤍 by <a href="https://suphakit.net" target="_blank">Suphakit P.</a>
+  </p>
+  <p>
+    &copy; 2023 <a href="https://suphakit.net" target="_blank">Suphakit P.</a> All Right Reserved.
+  </p>
 </div>`
 
 module.exports = Footer
