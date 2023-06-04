bd9e974d878a37e42ffefc895848c1ddb254f267
dethMastery <me@suphakit.net>
2023-06-04 11:31:44 +0700

    chore: html frame layout

--- src/static/_layout.js
+++ src/static/_layout.js
@@ -1,1 +1,18 @@
+function Layout(title, headBox, body) {
+  return html = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>${title}</title>
 
+    ${headBox}
+  </head>
+  <body>
+    ${body}
+  </body>
+</html>`
+}
+
+module.exports = Layout
