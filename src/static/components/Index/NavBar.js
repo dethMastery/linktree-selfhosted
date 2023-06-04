df5074b6ecf9f498b5bc4b8a9a461404a0635780
dethMastery <me@suphakit.net>
2023-06-04 17:50:55 +0700

    feat: navbar component

---
+++ src/static/components/Index/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = `<div id="navBar">
+  <div class="left">
+    <a href="/">katzuTree</a>
+  </div>
+  <div class="right">
+    <a href="/pricing">Pricing</a>
+    <a href="/register">Register</a>
+    <a href="/login">Login</a>
+  </div>
+</div>`
+
+module.exports = NavBar
