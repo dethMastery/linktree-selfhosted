0b29dbaa9178c9d4228eef9a9c151ce49d53ba28
Suphakit P <me@suphakit.net>
2023-06-24 21:40:45 +0700

    fix: mongo db connection error

--- src/API/mongo.js
+++ src/API/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const env = process.env
 
-const mongoURI = `mongodb://${env.MONGO_USER}:${env.MONGO_PASS}@${env.MONGO_URI}:${env.MONGO_PORT}/${MONGO_DB}?authSource=${MONGO_AUTH_SOURCE}`
+const mongoURI = `mongodb://${env.MONGO_USER}:${env.MONGO_PASS}@${env.MONGO_URI}:${env.MONGO_PORT}/${env.MONGO_DB}?authSource=${env.MONGO_AUTH_SOURCE}`
 
 mongoose.connect(mongoURI)
 const db = mongoose.connection
