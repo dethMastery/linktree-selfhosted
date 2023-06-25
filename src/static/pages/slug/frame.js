a1c04bee964b8e344453c3060770d95a89d5b430
Suphakit P <me@suphakit.net>
2023-06-25 11:29:33 +0700

    chore: prep for archived

---
+++ src/static/pages/slug/frame.js
@@ -0,0 +1,20 @@
+const NavBar = require("../components/Index/NavBar")
+const Footer = require("../components/Index/footer")
+
+const title = ' | katzuTree'
+const head = `<link rel="stylesheet" href="/css/pages/dashboard">`
+const body = `<!-- Top Heading -->
+<div class="big-heading mainScreen">
+  
+</div>
+
+<!-- Footer Component -->
+${Footer}`
+
+const dashboardComponents = {
+  title: title,
+  head: head,
+  body: body
+}
+
+module.exports = dashboardComponents
