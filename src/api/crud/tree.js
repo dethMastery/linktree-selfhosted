0868927707928e650713d74afefa3b4e3a94985b
Suphakit P <me@suphakit.net>
2023-06-28 14:09:25 +0700

    fix<tree adding>: duplicated data filtered

--- src/api/crud/tree.js
+++ src/api/crud/tree.js
@@ -6,64 +6,77 @@ require('../mongo')
 
 async function treeQuery(id) {
   const query = await model
-    .findOne({ id: id }, 'id slug title author timestamp links')
-    .exec()
+    .findOne({ id: id }, 'id slug title author timestamp links').sort({ id: 'desc' })
+
+  return query
+}
+
+async function slugQuery(author, slug) {
+  const query = await model
+    .findOne({ author: author, slug: slug }, 'id slug title author timestamp links').sort({ id: 'desc' })
 
   return query
 }
 
 async function latest() {
   const query = await model
-    .findOne({}, 'uID username')
-    .sort({ uID: 'desc' })
-    .exec()
+    .find({}, 'id slug title author timestamp links')
+    .sort({ id: 'desc' })
 
-  return query
+  return query[0]
 }
 
 async function createTree(data) {
   let res
 
   if (data != undefined) {
-    let linksArrays = []
+    if (await slugQuery(data.author, data.slug) == undefined) {
+      let linksArrays = []
 
-    for (let i = 0; i < data.links.length; i++) {
-      const tempData = {
-        id: parseInt(i) + 1,
-        service: data.links[i].services,
-        link: data.links[i].link,
+      for (let i = 0; i < data.links.length; i++) {
+        const tempData = {
+          id: parseInt(i),
+          service: data.links[i].services,
+          link: data.links[i].link,
+        }
+
+        linksArrays.push(tempData)
       }
 
-      linksArrays.push(tempData)
-    }
-    
-    let id
-    
-    if (await latest() == undefined) {
-      id = 1
+      let id
+
+      if (await latest() == undefined) {
+        id = 0
+      } else {
+        const last = await latest()
+        id = parseInt(last.id) + 1
+      }
+
+      const sendData = new model({
+        id: id,
+        title: data.title,
+        slug: data.slug,
+        author: data.author,
+        timestamp: tyDon(),
+        links: linksArrays,
+      })
+
+      try {
+        await sendData.save()
+        res = {
+          status: 200,
+          message: 'tree created successful!',
+          link: `/${data.author}/${data.slug}`,
+        }
+      } catch (err) {
+        console.error(err)
+        res = err
+      }
     } else {
-      id = parseInt(await latest().id) + 1
-    }
-    
-    const sendData = new model({
-      id: id,
-      title: data.title,
-      slug: data.slug,
-      author: data.author,
-      timestamp: tyDon(),
-      links: linksArrays,
-    })
-
-    try {
-      await sendData.save()
       res = {
-        status: 200,
-        message: 'tree created successful!',
-        link: `/${data.author}/${data.slug}`,
+        status: 403,
+        message: "duplicated data"
       }
-    } catch (err) {
-      console.error(err)
-      res = err
     }
   } else {
     res = {
