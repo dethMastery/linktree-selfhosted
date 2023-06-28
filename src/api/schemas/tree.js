b5c2ca689323869e57000d7428aaf54195bb287d
Suphakit P <me@suphakit.net>
2023-06-28 09:33:15 +0700

    feat: setup tree schemas

---
+++ src/api/schemas/tree.js
@@ -0,0 +1,36 @@
+let mongoose = require('mongoose')
+
+require('../mongo')
+
+const links = new mongoose.Schema({
+  id: {
+    require: true,
+    type: Number,
+  },
+  service: {
+    require: true,
+    type: String,
+  },
+  link: {
+    require: true,
+    type: String,
+  },
+})
+
+const trees = new mongoose.Schema({
+  id: {
+    require: true,
+    type: Number,
+  },
+  slug: {
+    require: true,
+    type: String,
+  },
+  title: {
+    require: true,
+    type: String,
+  },
+  links: [links],
+})
+
+module.exports = mongoose.model('trees', trees)
